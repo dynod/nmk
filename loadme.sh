ea5fa6346e09f77d33cd63960ca9799b2599a8af
David Pochet <david.pochet@gmail.com>
2022-02-24 17:23:09 +0100

    Give a try with github CI

--- loadme.sh
+++ loadme.sh
@@ -43,6 +43,7 @@ __installSysDeps() {
 # Check system dependencies
 __checkSysDeps git "git"
 __checkSysDeps python3 "python3 python3-venv"
+__checkSysDeps make "make"
 
 # Perform installs if needed
 __installSysDeps || return $?
